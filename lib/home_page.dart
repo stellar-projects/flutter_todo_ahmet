a62004f6264d300278564b25c69b023cb873e9f8
Ahmet Karayel <ahmetp.karayel06@gmail.com>
2022-10-01 16:32:55 +0300

    Fotoğraf ekleme aktif edildi.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 ///Yorumlar
-///Öncelikle eline sağlık, uygulama güzel çalışıyor. İlk aşamayı topaladığına göre mevcut yapıda birkaç geri bildirimim olucak. 
-/// 1- Kod yazarken her zaman aklında olması gereken ilk kural DRY prensibi, yani "Don't repeat yourself". İlk gözüme çarpan şey kimi parametrelerin tekrarlayan yapıca olması oldu. Örnek; 
+///Öncelikle eline sağlık, uygulama güzel çalışıyor. İlk aşamayı topaladığına göre mevcut yapıda birkaç geri bildirimim olucak.
+/// 1- Kod yazarken her zaman aklında olması gereken ilk kural DRY prensibi, yani "Don't repeat yourself". İlk gözüme çarpan şey kimi parametrelerin tekrarlayan yapıca olması oldu. Örnek;
 /// todo.keys.toList()[index] ve todo.values.toList()[index]  çağtıları. bunları listview builder'in en başında var key = todo.keys.toList()[index]; ve var isChecked = todo.values.toList()[index].
 /// şeklinde yazıp kodun kalan kısmında o şekilde kullanman daha doğru olur.
 /// 2- Değişken isimlendirmeleri: kod yazarken her zaman aklında senden sonra gelecek kişinin de o kodu okuması gerekebileceğini aklında tutman gerekiyor. o yüzden değişken isimlerin her zaman açıklayıcı olsun.
@@ -15,13 +18,12 @@ import 'package:flutter/material.dart';
 /// 8- daha doğru bir yaklaşım bu iş için bir map kullanmaktansa bir TodoItem class'ı oluşturup onların parametreleri üzerinden çalışmak olur.
 /// 9- Yine mevcut yapıda satırlarının "primary key" değerleri map olduğu için birden fazla aynı todo satırını kabul etmiyor. Primary key değerinin her zaman tek olmasını garnatilemen gerekiyor. Bu yüzden class yapısı + array şeklinde ilerlemen önemli.
 
-
-
 ///Todo listesi için class. Bu şekilde gelecekte eklenmesi muhtemel yeni parametreler kolayca sşsteme eklenebilir.
-class TodoItem{
+class TodoItem {
   String text;
   bool isChecked;
-  TodoItem(this.text, this.isChecked);
+  File? file;
+  TodoItem(this.text, this.isChecked, this.file);
 }
 
 class ToDoApp extends StatefulWidget {
@@ -32,54 +34,68 @@ class ToDoApp extends StatefulWidget {
 }
 
 class _ToDoAppState extends State<ToDoApp> {
-
-
-  final List<TodoItem> items = List.generate(4, (index) => TodoItem("Todo Item $index", false));
+  final List<TodoItem> items =
+      List.generate(4, (index) => TodoItem("Todo Item $index", false, null));
 
   TextEditingController userInput = TextEditingController();
 
   String text = "";
+  ImagePicker image = ImagePicker();
 
-  void _onTapDeleteRow(TodoItem item){
+  _onTapTakePhotoWithCamera(TodoItem item) async {
+    var img = await image.pickImage(source: ImageSource.camera);
+    setState(() {
+      item.file = File(img!.path);
+    });
+  }
+
+  _onTapSelectImageFromGallery(TodoItem item) async {
+    var img = await image.pickImage(source: ImageSource.gallery);
+    setState(() {
+      item.file = File(img!.path);
+    });
+  }
+
+  void _onTapDeleteRow(TodoItem item) {
     setState(() {
       items.remove(item);
     });
   }
 
-  void _onTapAddNewRow(String text){
+  void _onTapAddNewRow(String text) {
     setState(() {
-      items.add(TodoItem(text, false));
+      items.add(TodoItem(text, false, null));
       userInput.clear();
     });
     //print(todo);
   }
 
-  void _onTapUpdateRow(TodoItem item){
-      showDialog(
-        context: context,
-        builder: ((context) => SimpleDialog(
-          children: [
-            TextFormField(
-              onChanged: ((value) {
+  void _onTapUpdateRow(TodoItem item) {
+    showDialog(
+      context: context,
+      builder: ((context) => SimpleDialog(
+            children: [
+              TextFormField(
+                onChanged: ((value) {
                   text = value;
-              }),
-              initialValue: item.text,
-            ),
-            ElevatedButton(
-                onPressed: (() {
-                  setState(() {
-                    item.text = text;
-                  });
-                  Navigator.pop(context);
-                  //print(todo);
                 }),
-                child: const Text("Güncelle")),
-          ],
-        )),
-      );
+                initialValue: item.text,
+              ),
+              ElevatedButton(
+                  onPressed: (() {
+                    setState(() {
+                      item.text = text;
+                    });
+                    Navigator.pop(context);
+                    //print(todo);
+                  }),
+                  child: const Text("Güncelle")),
+            ],
+          )),
+    );
   }
 
-  void _onTapCheck(TodoItem item, bool isChecked){
+  void _onTapCheck(TodoItem item, bool isChecked) {
     setState(() {
       item.isChecked = isChecked;
     });
@@ -100,18 +116,62 @@ class _ToDoAppState extends State<ToDoApp> {
                     shape: RoundedRectangleBorder(
                         side: const BorderSide(color: Colors.teal, width: 3),
                         borderRadius: BorderRadius.circular(20.0)),
-                    title: Text(item.text,
-                        style: TextStyle(
-                            decoration: item.isChecked ? TextDecoration.lineThrough : null)),
+                    title: Row(
+                      children: [
+                        SizedBox(
+                          width: 100,
+                          child: Text(item.text,
+                              style: TextStyle(
+                                  decoration: item.isChecked
+                                      ? TextDecoration.lineThrough
+                                      : null)),
+                        ),
+                        const Spacer(),
+                        Column(
+                          children: [
+                            Container(
+                              width: 70,
+                              height: 70,
+                              color: Colors.black12,
+                              child: item.file == null
+                                  ? const Icon(Icons.image)
+                                  : Image.file(
+                                      item.file!,
+                                      fit: BoxFit.fill,
+                                    ),
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                    iconSize: 20,
+                                    onPressed: () {
+                                      _onTapTakePhotoWithCamera(item);
+                                    },
+                                    icon: const Icon(Icons.camera_alt)),
+                                IconButton(
+                                    iconSize: 20,
+                                    onPressed: () {
+                                      _onTapSelectImageFromGallery(item);
+                                    },
+                                    icon: const Icon(
+                                        Icons.photo_library_rounded)),
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
                     leading: Checkbox(
                       value: item.isChecked,
-                      onChanged: (newValue) => _onTapCheck(item, newValue ?? false),
+                      onChanged: (newValue) =>
+                          _onTapCheck(item, newValue ?? false),
                       activeColor: Colors.orange,
                     ),
                     trailing: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         IconButton(
+                          iconSize: 20,
                           color: Colors.red,
                           icon: const Icon(Icons.delete),
                           onPressed: () {
@@ -120,15 +180,19 @@ class _ToDoAppState extends State<ToDoApp> {
                           },
                         ),
                         IconButton(
+                          iconSize: 20,
                           color: Colors.black,
-                          onPressed: item.isChecked ? null : () => _onTapUpdateRow(item),
+                          onPressed: item.isChecked
+                              ? null
+                              : () => _onTapUpdateRow(item),
                           icon: const Icon(Icons.edit),
                         ),
                       ],
                     ),
                   );
                 },
-                separatorBuilder: (BuildContext context, int index) => const Divider(),
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
               ),
             ),
             Card(
@@ -144,7 +208,7 @@ class _ToDoAppState extends State<ToDoApp> {
                     ),
                   ),
                   ElevatedButton(
-                      onPressed: () =>_onTapAddNewRow(userInput.text),
+                      onPressed: () => _onTapAddNewRow(userInput.text),
                       child: const Text("Ekle"))
                 ],
               ),
@@ -153,5 +217,3 @@ class _ToDoAppState extends State<ToDoApp> {
         ));
   }
 }
-
-
